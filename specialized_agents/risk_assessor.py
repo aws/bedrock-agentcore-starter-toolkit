5d256dd4ded2db6f16c03d1d3e2b60ddadddc5a4
Blu-Chips <141764134+Blu-Chips@users.noreply.github.com>
2025-09-29 09:23:09 +0300

    commit_task4.2

---
+++ specialized_agents/risk_assessor.py
@@ -0,0 +1,15 @@
+rom enum ltdict
+fmport defauons im collectid
+fro, fieltaclasss import daassecle
+from data, Tupll, Anyona Optit, List,icort Dimpping om ty Decimal
+frport decimal ima
+fromme, timedeltmport datetietime i
+from datort mathgging
+imp
+import lo""
+ent.
+"anagemhold m risk threscators, andaud indiwn frs for knoe systems-referencoslysis,
+cr risk anand temporalic ang, geographscoririsk i-factor for multized agent 
+
+Specialtnt Agen Assessme"
+Risk""
