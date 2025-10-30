c63c9c839a4690f37b7356e2b61af5c4c0afda0a
Tejas Kashinath <tkashina@amazon.com>
2025-10-30 11:25:48 -0400

    Updating src paths, IaC directory creation and templates for Terraform

--- src/bedrock_agentcore_starter_toolkit/bootstrap/features/terraform/feature.py
+++ src/bedrock_agentcore_starter_toolkit/bootstrap/features/terraform/feature.py
@@ -7,7 +7,9 @@ class TerraformFeature(Feature):
     name = BootstrapIACProvider.Terraform.value
 
     def before_apply(self, context: ProjectContext):
-        context.iac_dir = context.output_dir
+        iac_dir = Path(context.output_dir / "terraform")
+        iac_dir.mkdir(exist_ok=False)
+        context.iac_dir = iac_dir
 
     def execute(self, context: ProjectContext) -> None:
         self.render_dir(context.iac_dir, context)
