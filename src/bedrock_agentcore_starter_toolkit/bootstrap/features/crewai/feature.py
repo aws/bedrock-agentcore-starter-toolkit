e4d43b09a515cfbaad1867adabcfa590f6dca215
Tejas Kashinath <tkashina@amazon.com>
2025-11-05 22:12:30 -0500

    fix: Fixes to CrewAI Agent Code

--- src/bedrock_agentcore_starter_toolkit/bootstrap/features/crewai/feature.py
+++ src/bedrock_agentcore_starter_toolkit/bootstrap/features/crewai/feature.py
@@ -4,7 +4,7 @@ from ..base_feature import Feature
 
 class CrewAIFeature(Feature):
     feature_dir_name = SDKProvider.CREWAI
-    python_dependencies = ["crewai[tools]>=1.3.0"]
+    python_dependencies = ["crewai[tools]>=1.3.0","crewai-tools[mcp]>=1.3.0","mcp>=1.20.0"]
 
     def execute(self, context: ProjectContext):
         self.render_dir(context.src_dir, context)
