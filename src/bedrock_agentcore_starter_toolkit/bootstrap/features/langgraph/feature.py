80eafd3f26c406f8e5041c5ffcab5e7fc0505f88
Tejas Kashinath <tkashina@amazon.com>
2025-11-06 11:50:02 -0500

    fix: langchain

--- src/bedrock_agentcore_starter_toolkit/bootstrap/features/langgraph/feature.py
+++ src/bedrock_agentcore_starter_toolkit/bootstrap/features/langgraph/feature.py
@@ -5,7 +5,7 @@ from ..base_feature import Feature
 class LangGraphFeature(Feature):
     feature_dir_name = SDKProvider.LANG_GRAPH
     python_dependencies = ["langgraph >= 1.0.2", "langchain_aws >= 1.0.0", "mcp >= 1.19.0",
-                           "langchain-mcp-adapters >= 0.1.11"]
+                           "langchain-mcp-adapters >= 0.1.11", "langchain >= 1.0.3"]
 
     def execute(self, context: ProjectContext):
         self.render_dir(context.src_dir, context)
