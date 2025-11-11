c5233b27d1ba0bec01da9fa2d2311009836e8808
notowen333 <owenmkaplan@gmail.com>
2025-11-11 10:15:02 -0500

    feat: revamp snapshot structure

--- tests/bootstrap/test_helper/run_bootstrap.py
+++ tests/bootstrap/test_helper/run_bootstrap.py
@@ -1,42 +1,35 @@
+from dataclasses import dataclass
 import shutil
 from pathlib import Path
 from typer.testing import CliRunner
+from .bootstrap_scenarios import ScenarioConfig, SCENARIOS
 
-from bedrock_agentcore_starter_toolkit.bootstrap.constants import (
-    SDKProvider
-)
 from bedrock_agentcore_starter_toolkit.cli.bootstrap.commands import (
     bootstrap_app,
 )
-from .syrupy_util import snapshot_dir_tree
 from unittest.mock import patch
 
-FIXTURES = Path(__file__).parent.parent / "fixtures"
-CONFIG_FIXTURES = FIXTURES / "bedrock_agentcore_config"
-SRC_FIXTURES = FIXTURES / "src_provided"
+FIXTURES = Path(__file__).parent.parent / "fixtures" / "scenarios"
 test_runner = CliRunner()
 
-# ---------------------------------------------------------------------------
-# Both cdk and terraform tests use this setup
-# Since only the IAC varies by scenario input, we only need to exercise each SDK at least once
-# ---------------------------------------------------------------------------
-SCENARIO_TO_SDK = {
-    "scenario_0": SDKProvider.STRANDS, # custom auth, stm and ltm, custom headers
-    "scenario_1": SDKProvider.OPENAI_AGENTS, # default settings: stm
-    "scenario_2": None, # Source is provided so there is no sdk provider
-}
-
-SCENARIO_WITH_SRC_PROVIDED = set(["scenario_2"])
-
-def run_bootstrap(tmp_path, monkeypatch, scenario, iac):
-    """Runs the CLI generator and returns the project directory."""
-    sdk = SCENARIO_TO_SDK[scenario]
+def run_bootstrap(tmp_path, monkeypatch, scenario, iac)-> tuple[Path, ScenarioConfig]:
+    """Runs the CLI generator and returns the project directory and the ScenarioConfig used"""
+    scenario_config = SCENARIOS[scenario]
+    sdk = scenario_config.sdk
 
     # Put the fixture into the working directory.
-    if scenario in SCENARIO_WITH_SRC_PROVIDED:
-        shutil.copytree(SRC_FIXTURES / scenario, tmp_path, dirs_exist_ok=True)
+    scenario_fixtures: Path = FIXTURES / scenario
+    provided_config_yaml = scenario_fixtures / ".bedrock_agentcore.yaml"
+    
+    if (scenario_fixtures / "src").is_dir():
+        # source provided scenario
+        shutil.copytree(scenario_fixtures, tmp_path, dirs_exist_ok=True)
+    elif provided_config_yaml.exists():
+        # config bootstrap mode scenario where there is a config file but no source code
+        shutil.copy(provided_config_yaml, tmp_path / ".bedrock_agentcore.yaml")
     else:
-        shutil.copy(CONFIG_FIXTURES / f"{scenario}.yaml", tmp_path / ".bedrock_agentcore.yaml")
+        # nothing was provided, run bootstrap without input
+        pass
     monkeypatch.chdir(tmp_path)
 
     project_name = "testProj"
@@ -61,5 +54,5 @@ def run_bootstrap(tmp_path, monkeypatch, scenario, iac):
         print("STDERR:", result.stderr)
     assert result.exit_code == 0
 
-    return tmp_path / project_name, sdk
+    return tmp_path / project_name, scenario_config
 
--- tests/bootstrap/test_terraform_bootstrap.py
+++ tests/bootstrap/test_terraform_bootstrap.py
@@ -5,13 +5,14 @@ from bedrock_agentcore_starter_toolkit.bootstrap.constants import (
 )
 
 from .test_helper.syrupy_util import snapshot_dir_tree
-from .test_helper.run_bootstrap import run_bootstrap, SCENARIO_TO_SDK
+from .test_helper.run_bootstrap import run_bootstrap
+from .test_helper.bootstrap_scenarios import SCENARIOS
 
 # ---------------------------------------------------------------------------
 # Terraform Snapshots
 # ---------------------------------------------------------------------------
 
-@pytest.mark.parametrize("scenario", list(SCENARIO_TO_SDK.keys()))
-def test_terraform_snapshots(snapshot, tmp_path, monkeypatch, scenario):
-    project_dir, sdk = run_bootstrap(tmp_path, monkeypatch, scenario, IACProvider.TERRAFORM)
-    assert snapshot_dir_tree(project_dir) == snapshot(name=f"{scenario}-{sdk}")
+@pytest.mark.parametrize("scenario", list(SCENARIOS.keys()))
+def test_cdk_snapshots(snapshot, tmp_path, monkeypatch, scenario):
+    project_dir, scenario_config = run_bootstrap(tmp_path, monkeypatch, scenario, IACProvider.TERRAFORM)
+    assert snapshot_dir_tree(project_dir) == snapshot(name=f"{scenario}-{scenario_config.sdk}-{scenario_config.description}")
